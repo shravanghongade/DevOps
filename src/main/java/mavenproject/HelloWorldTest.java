ae7b85d3bea2f5bb0512f3d4850724c33a0800b5
shravanghongade <shravanghongade@gmail.com>
2018-01-03 19:55:58 +0530

    This is modified locally

---
+++ src/main/java/mavenproject/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package mavenproject;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
